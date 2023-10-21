23e3a8ecadd903dd4bac141e45c22b732033637e
Prudence Au <prudence.au@gmail.com>
2023-10-20 21:33:29 -0400

    Added custom user profile model, manager and migrations

--- python_rest_api/settings.py
+++ python_rest_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_api'
+    'rest_api',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'rest_api.UserProfile'
--- rest_api/models.py
+++ rest_api/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profile"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        'make sure the password is stored as hash, encrypted'
+        user.set_password(password)
+
+        'saving django object in the database'
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with with a given details.
+        self is automatically passed in when the function is being called."""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    """replacing the default USERNAME_FIELD from AbstractBaseUser to email (ie.
+    instead of username, we would like an email)"""
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    'for django to interact with our custom model'
+    'defining a function within a class, self must be used as the argument'
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of the model.  Recommanded for
+        Django app"""
+        return self.email
